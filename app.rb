0eb8335c9727f4982aba61f710301e76576562de
Michael Fahim <michael_fahim@yahoo.com>
2020-06-20 19:19:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is Mike"
+  end
+  get '/hometown' do
+    "My hometown is Egypt"
+  end
+  get '/favorite-song' do
+    "My favorite song is ba7bek"
+  end
 end
